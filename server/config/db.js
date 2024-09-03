3bc782fd657cda72dc544579dee7d809bf3c08e0
CHIRAG TYAGI <69377702+chiragtyagi86@users.noreply.github.com>
2024-09-03 21:13:43 +0530

    done

---
+++ server/config/db.js
@@ -0,0 +1,28 @@
+// db.js
+require('dotenv').config();
+const mysql = require('mysql');
+
+// Load environment variables from.env file
+
+
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    
+  });
+  
+
+
+  connection.connect((err) => {
+
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as ID', connection.threadId);
+});
+
+module.exports = connection;
